f5a7a877b69c0bda336bfabf4b25786787070713
Jose Pereira <jose_miguel_pereira@hotmail.com>
2019-11-05 22:02:43 +0000

    A cube can track another cube!

--- Assets/Scripts/AIManager.cs
+++ Assets/Scripts/AIManager.cs
@@ -1,40 +1,79 @@
 ﻿using AI.DecisionTree;
 using System.Collections;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 
 public class AIManager : MonoBehaviour
 {
 
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject player = null;
+    [SerializeField] private GameObject enemy = null;
     [SerializeField] private float sightRadius = 50f;
+    [SerializeField] private float sightEndRadius = 10f;
+    [SerializeField] private float updateTimeStep = 0.2f;
 
     Node positive = new ActionNode(() => Debug.Log("Enemy in sight!"));
     Node negative = new ActionNode(() => Debug.Log("Enemy out of sight!"));
     Node root;
-    float timer = 0f;
 
     Coroutine aiCoroutine;
+    bool hit = false;
+    Vector3 gizmoPos = Vector3.zero;
+    float rayDistance = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        root = new BinaryDecisionNode(positive, negative, CheckDistance);
+        root = new BinaryDecisionNode(positive, negative, IsOnSight);
         aiCoroutine = StartCoroutine(AIFrame());
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(player.transform.position, sightRadius);
+
+        var dir = (enemy.transform.position - player.transform.position);
+        dir.Normalize();
+
+        if (hit) Gizmos.color = Color.green;
+        else Gizmos.color = Color.yellow;
+
+        Gizmos.DrawWireSphere(gizmoPos, sightEndRadius);
+        Gizmos.DrawRay(player.transform.position, dir * rayDistance);
     }
 
-    private bool CheckDistance()
-    {
-        float distance = Mathf.Abs(Vector3.Distance(player.transform.position, enemy.transform.position));
-        if (distance <= sightRadius) return true;
 
-        return false;
+    private bool IsOnSight()
+    {
+
+        var direction = enemy.transform.position - player.transform.position;
+        direction.Normalize();
+
+        RaycastHit raycastHit;
+
+        if (Physics.SphereCast(player.transform.position, sightEndRadius, direction, out raycastHit, sightRadius))
+        {
+            gizmoPos = raycastHit.point;
+            rayDistance = raycastHit.distance;
+
+            if (raycastHit.transform.gameObject == enemy)
+            {
+                hit = true;
+            }
+            else
+            {
+                hit = false;
+            }
+        }
+        else
+        {
+            gizmoPos = player.transform.position + direction * sightRadius;
+            rayDistance = sightRadius;
+            hit = false;
+        }
+
+        return hit;
+        
     }
 
     IEnumerator AIFrame()
@@ -44,7 +83,7 @@ public class AIManager : MonoBehaviour
         {
             root.Eval();
 
-            yield return new WaitForSecondsRealtime(0.2f);
+            yield return new WaitForSecondsRealtime(updateTimeStep);
         }
         
     }
