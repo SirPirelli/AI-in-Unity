863f6e5b3588432c62c74d1ac150f3a9728602eb
Jose Pereira <jose_miguel_pereira@hotmail.com>
2019-11-05 16:33:09 +0000

    Decision Tree Data Structures
    -Can check if an enemy is inside the sight radius

---
+++ Assets/Scripts/AIManager.cs
@@ -0,0 +1,51 @@
+﻿using AI.DecisionTree;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private float sightRadius = 50f;
+
+    Node positive = new ActionNode(() => Debug.Log("Enemy in sight!"));
+    Node negative = new ActionNode(() => Debug.Log("Enemy out of sight!"));
+    Node root;
+    float timer = 0f;
+
+    Coroutine aiCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        root = new BinaryDecisionNode(positive, negative, CheckDistance);
+        aiCoroutine = StartCoroutine(AIFrame());
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(player.transform.position, sightRadius);
+    }
+
+    private bool CheckDistance()
+    {
+        float distance = Mathf.Abs(Vector3.Distance(player.transform.position, enemy.transform.position));
+        if (distance <= sightRadius) return true;
+
+        return false;
+    }
+
+    IEnumerator AIFrame()
+    {
+
+        while(true)
+        {
+            root.Eval();
+
+            yield return new WaitForSecondsRealtime(0.2f);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/DecisionTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AI.DecisionTree
+{
+    abstract class Node
+    {
+        public virtual void Eval() { }
+    }
+    
+    class BinaryDecisionNode : Node
+    {
+        private Node trueChild, falseChild;
+        Func<bool> condition;
+
+        public BinaryDecisionNode(Node trueChild, Node falseChild, Func<bool> condition)
+        {
+            this.trueChild = trueChild;
+            this.falseChild = falseChild;
+            this.condition = condition;
+        }
+
+        public override void Eval()
+        {
+
+            if (condition())
+                trueChild.Eval();
+            else
+                falseChild.Eval();
+        }
+    }
+
+    class ActionNode : Node
+    {
+        Action action;
+
+        public ActionNode(Action action)
+        {
+            this.action = action;
+        }
+
+        public override void Eval()
+        {
+            action();
+        }
+    }
+}
