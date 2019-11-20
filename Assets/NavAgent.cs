a1577cec04eb06ed868afd4fb396586628706ea6
Jose Pereira <jose_miguel_pereira@hotmail.com>
2019-11-20 17:21:16 +0000

    NavMesh

---
+++ Assets/NavAgent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgent : MonoBehaviour
+{
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var agent = GetComponent<NavMeshAgent>().SetDestination(target.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
