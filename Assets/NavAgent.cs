261d6da3bacb9310da242e4347d3c3b12cadcaf8
Jose Pereira <jose_miguel_pereira@hotmail.com>
2019-11-22 18:44:19 +0000

    if bots dont see player, go to random place

--- Assets/NavAgent.cs
+++ Assets/NavAgent.cs
@@ -5,18 +5,57 @@ public class NavAgent : MonoBehaviour
 {
 
     public Transform target;
+    public NavMeshAgent navMeshAgent;
+
+    public bool IsChasingTarget { get; set; }
+
+    private Vector3 destination;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        //var agent = GetComponent<NavMeshAgent>().SetDestination(target.position);
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        IsChasingTarget = false;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void ChaseTarget()
     {
+        navMeshAgent.SetDestination(target.position);
+        IsChasingTarget = true;
+
+        destination = target.position;
+    }
+
+    public bool SetDestination(Vector3 destination)
+    {
+        navMeshAgent.SetDestination(destination);
+        IsChasingTarget = false;
+        this.destination = destination;
+
+        if (navMeshAgent.pathStatus != NavMeshPathStatus.PathComplete)
+        { Debug.Log("REsturend null!"); return false; }
+
+        return true;
+
+    }
+
+    public bool HasReachedDestination()
+    {
+        //NavMeshHit navMeshHit;
+        //var b = navMeshAgent.SamplePathPosition(-1, navMeshAgent.radius, out navMeshHit);
+
+        return Mathf.Abs(navMeshAgent.remainingDistance) <= navMeshAgent.radius ? true : false;        
+
         
     }
 
-    public void ChaseTarget() { var agent = GetComponent<NavMeshAgent>().SetDestination(target.position); }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        if(destination != null)
+            Gizmos.DrawCube(destination, Vector3.one * 20f);
+    }
+
 }
--- Assets/Scripts/AIManager.cs
+++ Assets/Scripts/AIManager.cs
@@ -5,16 +5,20 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIManager : MonoBehaviour
 {
 
     [SerializeField] private Transform target = null;
     [SerializeField] private List<GameObject> enemies = null;
-    [Range(0, 500)]
-    [SerializeField] private float viewDistance = 50f;
-    [Range(0, 360)]
-    [SerializeField] private float fov = 90f;
+    [Range(0, 300)][SerializeField] private float speed = 0f;
+    [Range(0, 300)] [SerializeField] private float acceleration = 0f;
+    [Range(0, 300)] [SerializeField] private float angularSpeed = 0f;
+    [Range(0, 300)] [SerializeField] private float stoppingDistance = 0f;
+    [Range(0, 500)][SerializeField] private float viewDistance = 50f;
+    [Range(0, 360)][SerializeField] private float fov = 90f;
+
     [SerializeField] private float updateTimeStep = 0.2f;
 
     Node positive = new ActionNode(() => Debug.Log("Enemy in sight!"));
@@ -25,6 +29,20 @@ public class AIManager : MonoBehaviour
 
     Dictionary<GameObject, Node> enemiesDecisionTree;
 
+    private void OnValidate()
+    {
+        if(enemiesDecisionTree != null)
+        foreach (var item in enemiesDecisionTree)
+        {
+            var agent = item.Key.GetComponent<NavAgent>();
+            agent.navMeshAgent.speed = speed;
+            agent.navMeshAgent.acceleration = acceleration;
+            agent.navMeshAgent.angularSpeed = angularSpeed;
+            agent.navMeshAgent.stoppingDistance = stoppingDistance;
+
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,17 +51,26 @@ public class AIManager : MonoBehaviour
 
         foreach (var enemy in enemies)
         {
+            var navAgent = enemy.GetComponent<NavAgent>();
+            navAgent.navMeshAgent.speed = speed;
+            navAgent.navMeshAgent.angularSpeed = angularSpeed;
+            navAgent.navMeshAgent.acceleration = acceleration;
+            navAgent.navMeshAgent.stoppingDistance = stoppingDistance;
+
             //initialize conditions
+            ArgCondition<NavAgent> hasReachedDestination = new ArgCondition<NavAgent>(HasReachedDestination, navAgent);
             ArgCondition<Transform> viewDistanceNode = new ArgCondition<Transform>(IsInViewDistance, enemy.transform);
             ArgCondition<Transform> coneOfViewNode = new ArgCondition<Transform>(IsInConeOfView, enemy.transform);
             ArgCondition<Transform> clearSightNode = new ArgCondition<Transform>(IsInClearSight, enemy.transform);
 
-            ArgAction<NavAgent> startChaseAction = new ArgAction<NavAgent>(StartChase, enemy.GetComponent<NavAgent>());
+            ArgAction<NavAgent> startChaseAction = new ArgAction<NavAgent>(StartChase, navAgent);
+            ArgAction<NavAgent> setRandomTarget = new ArgAction<NavAgent>(SetRandomTarget, navAgent);
 
             //initialize nodes
-            Node thirdLayer = new BinaryDecisionNode(startChaseAction, negative, clearSightNode);
-            Node secondLayer = new BinaryDecisionNode(thirdLayer, negative, coneOfViewNode);
-            Node root = new BinaryDecisionNode(secondLayer, negative, viewDistanceNode);
+            Node hasReachedDestNode = new BinaryDecisionNode(setRandomTarget, negative, hasReachedDestination);
+            Node isInSight = new BinaryDecisionNode(startChaseAction, hasReachedDestNode, clearSightNode);
+            Node isInConeOfView = new BinaryDecisionNode(isInSight, hasReachedDestNode, coneOfViewNode);
+            Node root = new BinaryDecisionNode(isInConeOfView, hasReachedDestNode, viewDistanceNode);
 
             enemiesDecisionTree.Add(enemy, root);
 
@@ -56,6 +83,7 @@ public class AIManager : MonoBehaviour
     {
         foreach (var item in enemies)
         {
+            Handles.color = Color.white;
             Handles.DrawWireArc(item.transform.position, item.transform.up, item.transform.forward, 360, viewDistance);
             Handles.color = Color.yellow;
             var fovA = MathExtension.GetDirectionFromAngle(item.transform.eulerAngles.y + (-fov / 2));
@@ -66,11 +94,36 @@ public class AIManager : MonoBehaviour
         }
     }
 
+    private bool IsChasingTarget(NavAgent navAgent)
+    {
+        return navAgent.IsChasingTarget;
+    }
+
+    private void SetRandomTarget(NavAgent navAgent)
+    {
+
+        Vector2 point = Vector2.zero;
+        Vector3 destination = Vector3.zero;
+
+        do
+        {
+
+            point = Random.insideUnitCircle;
+            destination = new Vector3(point.x, 0, point.y) * viewDistance;
+
+        } while (!navAgent.SetDestination(destination));
+
+    }
+
     private void StartChase(NavAgent navAgent)
     {
         navAgent.ChaseTarget();
     }
 
+    private bool HasReachedDestination(NavAgent navAgent)
+    {
+        return navAgent.HasReachedDestination();
+    }
 
     private bool IsInViewDistance(Transform transform)
     {
